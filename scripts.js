ffe444d504e6fbaa6264444ea247f0c928ca3e7d
Joakim Jönsson <jockejockejockejocke@gmail.com>
2022-08-20 19:30:12 +0200

    Created checkWinner, playRound och game function.

--- scripts.js
+++ scripts.js
@@ -1,18 +1,64 @@
 let choiches = ["Rock", "Paper", "Scissor"];
+let scorePlayer = 0;
+let scoreComputer = 0;
 
+// Random Computer Choiche
 function getComputerChoice() {
-  const randomIndex = Math.floor(Math.random() * choiches.length);
-
-  const item = choiches[randomIndex];
-
-  return item;
+  return Math.floor(Math.random() * choiches.length);
 }
-console.log(getComputerChoice());
 
-
+// A round of play
 function playRound(playerSelection, computerSelection) {
-if(playerSelection == "Rock")
+  if (playerSelection === "ROCK" && computerSelection === 2) {
+    scorePlayer++;
+    return "You Win! Rock beats Scissors!";
+  } else if (playerSelection === "ROCK" && computerSelection === 1) {
+    scoreComputer++;
+    return "You loose. Paper beats rock.";
+  } else if (playerSelection === "ROCK" && computerSelection === 0) {
+    return "It´s a draw!";
+  } else if (playerSelection === "PAPER" && computerSelection === 2) {
+    scoreComputer++;
+    return "You loose! Paper beats scissor!";
+  } else if (playerSelection === "PAPER" && computerSelection === 1) {
+    return "It´s a draw!";
+  } else if (playerSelection === "PAPER" && computerSelection === 0) {
+    scorePlayer++;
+    return "You win! Paper beats rock!";
+  } else if (playerSelection === "SCISSOR" && computerSelection === 2) {
+    return "It´s a draw!";
+  } else if (playerSelection === "SCISSOR" && computerSelection === 1) {
+    scorePlayer++;
+    return "You win! Scissor beats paper!";
+  } else if (playerSelection === "SCISSOR" && computerSelection === 0) {
+    scoreComputer++;
+    return "You lose! Rock beats scissor!";
+  }
 }
 
-const playerSelection = 'Rock';
-const computerSelection = getComputerChoice();
+function game() {
+  for (let i = 0; i < 5; i++) {
+    const computerSelection = getComputerChoice();
+    const playerSelection = prompt(
+      'Please choose between "Rock, Paper or Scissor'
+    ).toUpperCase();
+    console.log(playRound(playerSelection, computerSelection));
+    console.log(`Your score ${scorePlayer}. Computers score: ${scoreComputer}`);
+  }
+  //Call check winner function
+  checkWinner();
+}
+
+function checkWinner() {
+  if (scorePlayer > scoreComputer) {
+    console.log(
+      `You won! Your score was ${scorePlayer} computers score was ${scoreComputer}.`
+    );
+  } else {
+    console.log(
+      `You lost! Your score was ${scorePlayer} and the computers score was ${scoreComputer}.`
+    );
+  }
+}
+
+game();
