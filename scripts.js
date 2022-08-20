713e047e8129ec8b0399abced4036e63575395f4
Joakim Jönsson <jockejockejockejocke@gmail.com>
2022-08-20 08:43:22 +0200

    Created index.html and scripts.js. Created function getComputerChoice.

---
+++ scripts.js
@@ -0,0 +1,18 @@
+let choiches = ["Rock", "Paper", "Scissor"];
+
+function getComputerChoice() {
+  const randomIndex = Math.floor(Math.random() * choiches.length);
+
+  const item = choiches[randomIndex];
+
+  return item;
+}
+console.log(getComputerChoice());
+
+
+function playRound(playerSelection, computerSelection) {
+if(playerSelection == "Rock")
+}
+
+const playerSelection = 'Rock';
+const computerSelection = getComputerChoice();
